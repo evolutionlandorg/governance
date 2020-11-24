864a062346ea7a6d42c2620f3f6b5b7912171145
xiaocheng <xiaoch2010@gmail.com>
2020-11-24 22:24:59 +0800

    deploy script: select network id

--- deploy-contract.js
+++ deploy-contract.js
@@ -8,12 +8,39 @@ const config = require('./compile/config.json')
 var readlineSync = require('readline-sync');
 var deploy = require("./compile/deploy.js")
 
+const networks = [
+    {
+        "id": 1,
+        "name": "ropsten(default)",
+        "provider": config.ropsten_provider
+    },
+    {
+        "id": 2,
+        "name": "rinkeby",
+        "provider": config.rinkeby_provider
+    },
+    {
+        "id": 3,
+        "name": "mainnet",
+        "provider": config.mainnet_provider
+    }
+];
+
 const main = async function() {
     var contract = readlineSync.question('which to be deployed?\r\n 1 - EvolutionTeller(default)\r\n 2 - SnapshotProxy\r\n');
-    var network = readlineSync.question('which network?\r\n 1 - rinkeby(default)\r\n 2 - mainnet\r\n');
+    var helper = 'which network?\r\n';
+    networks.forEach(function(option) {
+        helper = helper + option.id + " - " + option.name + "\r\n";
+    });
+    //var network = readlineSync.question('which network?\r\n 1 - rinkeby(default)\r\n 2 - mainnet\r\n');
+    var network = readlineSync.question(helper);
 
     const contractname = contract == 1 || contract == "" ? "EvolutionTeller" : "SnapshotProxy";
-    const networkname = network == 1 || network == "" ? config.rinkeby_provider : config.mainnet_provider;
+    //const networkname = network == 1 || network == "" ? config.rinkeby_provider : config.mainnet_provider;
+    const web3 = new Web3(networks[network-1].provider);
+    const key =  config.key;
+    web3.eth.accounts.wallet.add(key);
+    const networkname = networks[network-1].name;
     
     if (contractname == "EvolutionTeller") {
         var registry = readlineSync.question("Please enter the registry contract address\r\n");
